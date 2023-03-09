502e5aa8c9bd398e35c91684df4859d06a720c73
hendrawitono <hendra.witono@gmail.com>
2023-03-09 09:44:56 +0700

    first commit

---
+++ lib/bindings/initial_binding.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+import 'package:tugasfinal/controllers/controllers.dart';
+import 'package:tugasfinal/services/services.dart';
+
+class InitialBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.put(ThemeController());
+    //Get.put(PapersDataUploader());
+    Get.put(AuthController(), permanent: true);
+    Get.put(NotificationService());
+    Get.lazyPut(() =>  FireBaseStorageService());
+  }
+}
